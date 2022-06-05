52969694f95509616835fbec44cbc5ea027bf299
JMurteira267 <josemiguelmurteira@gmail.com>
2022-06-05 16:28:09 +0100

    Codigo inicial para plucked string
    tem o karplus strong algorythm e o inicio do expanded one

---
+++ string matalb/callidk.m
@@ -0,0 +1,25 @@
+Fs=44000
+Ts=1/Fs
+A=440
+delay=round(Fs/A)
+b  = firls(42, [0 1/delay 2/delay 1], [0 0 1 1]);
+a  = [1 zeros(1, delay) -0.5 -0.5];
+x = zeros(Fs*4, 1);
+
+S=1.5
+B=0.7 %[0,1]
+P=delay/Fs
+g_cte=exp(-6.91*P/S)
+g0=g_cte*(1+B)/2
+g1=g_cte*(1-B)/4
+
+data=sim('string_perhaps.slx',2)
+note=data.simout.Data
+%note=squeeze(data.simout.Data(1,1,:))
+%zi = rand(max(length(b),length(a))-1,1);
+%note = filter(b, a, x, zi);
+note = note-mean(note);
+note = note/max(abs(note))
+%sound(10000*squeeze(data.simout.Data(1,1,:)),44000)
+sound(4*note,Fs)
+audiowrite("THESOUND.wav",note,Fs)
